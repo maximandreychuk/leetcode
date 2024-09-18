2db5e7ca4b04138aefa31314ca5ed013c54e48c3
Максим Андрейчук <maximandreychuk1999@gmail.com>
2024-09-18 14:24:07 +0300

    git init

--- recurs/main.py
+++ recurs/main.py
@@ -1,15 +1,2 @@
-step = 0
-
-def persistence(n):
-    global step
-    lst = [int(i) for i in str(n)]
-    if len(lst) == 1:
-        return step // 2
-    else:
-        res = 1 * lst[0]
-        lst = lst[1:]
-        step += 1
-        while len(lst) != 0:
-            res = res * lst[0]
-            lst = lst[1:]
-        return persistence(res)
+def check_time(func):
+    def wrapper(*args, **kwargs):
