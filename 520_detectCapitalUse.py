b3e236dbea17f56f91dad789547a8314ccb1090b
Максим Андрейчук <maximandreychuk1999@gmail.com>
2024-11-02 14:11:28 +0300

    resolve detectCapitalUse

---
+++ 520_detectCapitalUse.py
@@ -0,0 +1,21 @@
+def detectCapitalUse(word):
+    if word[0] == word[0].upper() and word[1] != word[1].upper():
+        for i in range(2, len(word)):
+            if word[i] == word[i].upper():
+                return False
+    if word[0] == word[0].upper() and word[1] == word[1].upper():
+        for i in range(2, len(word)):
+            if word[i] != word[i].upper():
+                return False
+    if word[0] == word[0].lower() and word[1] == word[1].lower():
+        for i in range(2, len(word)):
+            if word[i] != word[i].lower():
+                return False
+    if word[0] == word[0].lower() and word[1] != word[1].lower():
+        return False
+    return True
+
+print(detectCapitalUse("USA"))
+print(detectCapitalUse("FlaG"))
+print(detectCapitalUse("Leetcode"))
+print(detectCapitalUse("FFFFFFFFFFFFFFFFFFFFf"))
